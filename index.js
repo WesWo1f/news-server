13824a201ed854590d92c3be12e45be7963f439d
Wes <claude.w.fountain@gmail.com>
2022-12-28 15:21:08 -0500

    setting up/ connecting backend to front

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-const express = require("express");
+const PORT = 8000
+const express = require('express')
+const cors = require('cors')
+const axios = require('axios')
+require('dotenv').config()
 
-const PORT = process.env.PORT || 3001;
+const app = express()
 
-const app = express();
+app.use(cors());
+app.get('/',(req,res) =>  {
+    res.json({name: "wes", key: process.env.SECRET_KEY })
+    console.log(process.env.SECRET_KEY)
+})
 
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
-});
+
+
+app.listen(8000, () => console.log(`Server is running on port ${PORT}`))
+
+
+
