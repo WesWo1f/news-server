d0a88878f3f045c9b51268bfcde96c785b22ca94
Wes <claude.w.fountain@gmail.com>
2023-01-16 14:51:00 -0500

    testing server response

--- index.js
+++ index.js
@@ -8,96 +8,141 @@ const app = express()
 const bodyParser = require('body-parser');
 app.use(express.json())
 app.use(cors());
+
+
+
+
+
 app.post('/category', async (req,res) =>  {
-  const category =  req.body.category
-  console.log(category)
-    const options = {
-        method: 'GET',
-        url: 'https://bing-news-search1.p.rapidapi.com/news/search',
-        params: {
-        q: `${category}`,
-        count: '100',
-        freshness: 'Day', 
-        textFormat: 'Raw', 
-        safeSearch: 'Off'},
-        headers: {
-          'X-BingApis-SDK': 'true',
-          'X-RapidAPI-Key': process.env.THE_KEY,
-          'X-RapidAPI-Host': process.env.THE_HOST
-        }
-      };
-      axios.request(options).then(function (response) {
-        res.json({name: "wes", theNews: response.data})
-          //console.log(response.data);
-      }).catch(function (error) {
-          console.error(error);
-      });
-
     console.log("category ran!!")
-    //console.log("this is options: "+options)
-})
+    //const category =  req.body.category
+    //console.log(category)
+      // const options = {
+      //     method: 'GET',
+      //     url: 'https://bing-news-search1.p.rapidapi.com/news/search',
+      //     params: {
+      //     q: `${category}`,
+      //     count: '100',
+      //     freshness: 'Day', 
+      //     textFormat: 'Raw', 
+      //     safeSearch: 'Off'},
+      //     headers: {
+      //       'X-BingApis-SDK': 'true',
+      //       'X-RapidAPI-Key': process.env.THE_KEY,
+      //       'X-RapidAPI-Host': process.env.THE_HOST
+      //     }
+      //   };
+        // axios.request(options).then(function (response) {
+        //   res.json({name: "wes", theNews: response.data})
+        //     //console.log(response.data);
+        // }).catch(function (error) {
+        //     console.error(error);
+        // });
+        res.send('Hello World')
+      console.log("category ran!!")
+      //console.log("this is options: "+options)
+  })
 
 
 
-app.post('/search', async (req,res) =>  {
-  const search =  req.body.search
-  console.log(search)
-    const options = {
-        method: 'GET',
-        url: 'https://bing-news-search1.p.rapidapi.com/news/search',
-        params: {
-        q: `${search}`,
-        count: '100',
-        freshness: 'Day', 
-        textFormat: 'Raw', 
-        safeSearch: 'Off'},
-        headers: {
-          'X-BingApis-SDK': 'true',
-          'X-RapidAPI-Key': process.env.THE_KEY,
-          'X-RapidAPI-Host': process.env.THE_HOST
-        }
-      };
+
+
+
+
+
+
+
+
+// app.post('/category', async (req,res) =>  {
+//   const category =  req.body.category
+//   console.log(category)
+//     const options = {
+//         method: 'GET',
+//         url: 'https://bing-news-search1.p.rapidapi.com/news/search',
+//         params: {
+//         q: `${category}`,
+//         count: '100',
+//         freshness: 'Day', 
+//         textFormat: 'Raw', 
+//         safeSearch: 'Off'},
+//         headers: {
+//           'X-BingApis-SDK': 'true',
+//           'X-RapidAPI-Key': process.env.THE_KEY,
+//           'X-RapidAPI-Host': process.env.THE_HOST
+//         }
+//       };
+//       axios.request(options).then(function (response) {
+//         res.json({name: "wes", theNews: response.data})
+//           //console.log(response.data);
+//       }).catch(function (error) {
+//           console.error(error);
+//       });
+
+//     console.log("category ran!!")
+//     //console.log("this is options: "+options)
+// })
+
+
+
+// app.post('/search', async (req,res) =>  {
+//   const search =  req.body.search
+//   console.log(search)
+//     const options = {
+//         method: 'GET',
+//         url: 'https://bing-news-search1.p.rapidapi.com/news/search',
+//         params: {
+//         q: `${search}`,
+//         count: '100',
+//         freshness: 'Day', 
+//         textFormat: 'Raw', 
+//         safeSearch: 'Off'},
+//         headers: {
+//           'X-BingApis-SDK': 'true',
+//           'X-RapidAPI-Key': process.env.THE_KEY,
+//           'X-RapidAPI-Host': process.env.THE_HOST
+//         }
+//       };
       
-      axios.request(options).then(function (response) {
-        res.json({name: "wes", theNews: response.data})
-          //console.log(response.data);
-      }).catch(function (error) {
-          console.error(error);
-      });
+//       axios.request(options).then(function (response) {
+//         res.json({name: "wes", theNews: response.data})
+//           //console.log(response.data);
+//       }).catch(function (error) {
+//           console.error(error);
+//       });
 
-    console.log("search ran!!")
-    //console.log("this is options: "+options)
+//     console.log("search ran!!")
+//     //console.log("this is options: "+options)
 
-})
+// })
 
-app.post('/trending',  async (req,res) =>  {
-  const search =  req.body.search
-  console.log(search)
-    const options = {
-        method: 'GET',
-        url: 'https://bing-news-search1.p.rapidapi.com/news/trendingtopics',
-        params: {
-        count: '100',
-        textFormat: 'Raw', 
-        safeSearch: 'Off'},
-        headers: {
-          'X-BingApis-SDK': 'true',
-          'X-RapidAPI-Key': process.env.THE_KEY,
-          'X-RapidAPI-Host': process.env.THE_HOST
-        }
-      };
+// app.post('/trending',  async (req,res) =>  {
+//   const search =  req.body.search
+//   console.log(search)
+//     const options = {
+//         method: 'GET',
+//         url: 'https://bing-news-search1.p.rapidapi.com/news/trendingtopics',
+//         params: {
+//         count: '100',
+//         textFormat: 'Raw', 
+//         safeSearch: 'Off'},
+//         headers: {
+//           'X-BingApis-SDK': 'true',
+//           'X-RapidAPI-Key': process.env.THE_KEY,
+//           'X-RapidAPI-Host': process.env.THE_HOST
+//         }
+//       };
       
-      axios.request(options).then(function (response) {
-        res.json({theNews: response.data})
-          //console.log(response.data);
-      }).catch(function (error) {
-          console.error(error);
-      });
+//       axios.request(options).then(function (response) {
+//         res.json({theNews: response.data})
+//           //console.log(response.data);
+//       }).catch(function (error) {
+//           console.error(error);
+//       });
 
-    console.log("trending ran!!")
-    //console.log("this is options: "+options)
+//     console.log("trending ran!!")
+//     //console.log("this is options: "+options)
 
-})
+// })
 
 
 
