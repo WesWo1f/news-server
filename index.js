3004a608380795684425a17629108f92282970d8
Wes <claude.w.fountain@gmail.com>
2023-01-16 16:43:59 -0500

    testing api call to rapidapi

--- index.js
+++ index.js
@@ -11,40 +11,40 @@ const bodyParser = require('body-parser');
 //app.use(cors());
 app.use(cors())
 
-app.post('/category', async (req,res) => {
-  res.json({ name:'wes'})
-});
+// app.post('/category', async (req,res) => {
+//   res.json({ name:'wes'})
+// });
 
 
 
-// app.post('/category', async (req,res) =>  {
-//     console.log("category ran!!")
-//     //const category =  req.body.category
-//     //console.log(category)
-//       // const options = {
-//       //     method: 'GET',
-//       //     url: 'https://bing-news-search1.p.rapidapi.com/news/search',
-//       //     params: {
-//       //     q: `${category}`,
-//       //     count: '100',
-//       //     freshness: 'Day', 
-//       //     textFormat: 'Raw', 
-//       //     safeSearch: 'Off'},
-//       //     headers: {
-//       //       'X-BingApis-SDK': 'true',
-//       //       'X-RapidAPI-Key': process.env.THE_KEY,
-//       //       'X-RapidAPI-Host': process.env.THE_HOST
-//       //     }
-//       //   };
-//         // axios.request(options).then(function (response) {
-//         //   res.json({name: "wes", theNews: response.data})
-//         //     //console.log(response.data);
-//         // }).catch(function (error) {
-//         //     console.error(error);
-//         // });
-//       console.log("category ran!!")
-//       //console.log("this is options: "+options)
-//   })
+app.post('/category', async (req,res) =>  {
+    console.log("category ran!!")
+    const category =  req.body.category
+      console.log(category)
+        const options = {
+        method: 'GET',
+        url: 'https://bing-news-search1.p.rapidapi.com/news/search',
+        params: {
+        q: `${category}`,
+        count: '100',
+        freshness: 'Day', 
+        textFormat: 'Raw', 
+        safeSearch: 'Off'},
+        headers: {
+          'X-BingApis-SDK': 'true',
+          'X-RapidAPI-Key': process.env.THE_KEY,
+          'X-RapidAPI-Host': process.env.THE_HOST
+        }
+      };
+      axios.request(options).then(function (response) {
+        res.json({name: "wes", theNews: response.data})
+          //console.log(response.data);
+      }).catch(function (error) {
+          console.error(error);
+      });
+      console.log("category ran!!")
+      console.log("this is options: "+options)
+  })
 
 
 
