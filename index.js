db4f03bc7d0d780531bdc6632821bfc3b8858f28
Wes <claude.w.fountain@gmail.com>
2023-01-16 13:57:25 -0500

    added endpoint trending

--- index.js
+++ index.js
@@ -1,16 +1,106 @@
 const PORT = 8000
 const express = require('express')
 const cors = require('cors')
-const axios = require('axios')
+const axios = require("axios");
 require('dotenv').config()
 
 const app = express()
-
+const bodyParser = require('body-parser');
+app.use(express.json())
 app.use(cors());
-app.get('/',(req,res) =>  {
-    res.json({name: "wes", key: process.env.SECRET_KEY })
-    console.log(process.env.SECRET_KEY)
+app.post('/category', async (req,res) =>  {
+  const category =  req.body.category
+  console.log(category)
+    const options = {
+        method: 'GET',
+        url: 'https://bing-news-search1.p.rapidapi.com/news/search',
+        params: {
+        q: `${category}`,
+        count: '100',
+        freshness: 'Day', 
+        textFormat: 'Raw', 
+        safeSearch: 'Off'},
+        headers: {
+          'X-BingApis-SDK': 'true',
+          'X-RapidAPI-Key': process.env.THE_KEY,
+          'X-RapidAPI-Host': process.env.THE_HOST
+        }
+      };
+      axios.request(options).then(function (response) {
+        res.json({name: "wes", theNews: response.data})
+          //console.log(response.data);
+      }).catch(function (error) {
+          console.error(error);
+      });
+
+    console.log("category ran!!")
+    //console.log("this is options: "+options)
 })
+
+
+
+app.post('/search', async (req,res) =>  {
+  const search =  req.body.search
+  console.log(search)
+    const options = {
+        method: 'GET',
+        url: 'https://bing-news-search1.p.rapidapi.com/news/search',
+        params: {
+        q: `${search}`,
+        count: '100',
+        freshness: 'Day', 
+        textFormat: 'Raw', 
+        safeSearch: 'Off'},
+        headers: {
+          'X-BingApis-SDK': 'true',
+          'X-RapidAPI-Key': process.env.THE_KEY,
+          'X-RapidAPI-Host': process.env.THE_HOST
+        }
+      };
+      
+      axios.request(options).then(function (response) {
+        res.json({name: "wes", theNews: response.data})
+          //console.log(response.data);
+      }).catch(function (error) {
+          console.error(error);
+      });
+
+    console.log("search ran!!")
+    //console.log("this is options: "+options)
+
+})
+
+app.post('/trending',  async (req,res) =>  {
+  const search =  req.body.search
+  console.log(search)
+    const options = {
+        method: 'GET',
+        url: 'https://bing-news-search1.p.rapidapi.com/news/trendingtopics',
+        params: {
+        count: '100',
+        textFormat: 'Raw', 
+        safeSearch: 'Off'},
+        headers: {
+          'X-BingApis-SDK': 'true',
+          'X-RapidAPI-Key': process.env.THE_KEY,
+          'X-RapidAPI-Host': process.env.THE_HOST
+        }
+      };
+      
+      axios.request(options).then(function (response) {
+        res.json({theNews: response.data})
+          //console.log(response.data);
+      }).catch(function (error) {
+          console.error(error);
+      });
+
+    console.log("trending ran!!")
+    //console.log("this is options: "+options)
+
+})
+
+
+
 
 
 
