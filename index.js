afd7eb07dc17591a69b44aea89f1d716d1d7c91e
Wes <claude.w.fountain@gmail.com>
2023-01-17 16:35:29 -0500

    added endpoint

--- index.js
+++ index.js
@@ -10,9 +10,33 @@ const bodyParser = require('body-parser');
 app.use(express.json())
 app.use(cors())
 
-// app.post('/category', async (req,res) => {
-//   res.json({ name:'wes',password:`${process.env.SECRET_KEY}`})
-// });
+app.post('/', async (req,res) => {
+  const options = {
+    method: 'GET',
+    url: 'https://bing-news-search1.p.rapidapi.com/news',
+    params: {safeSearch: 'Off', textFormat: 'Raw'},
+    headers: {
+      'X-BingApis-SDK': 'true',
+      'X-RapidAPI-Key': process.env.THE_KEY,
+      'X-RapidAPI-Host': process.env.THE_HOST
+    }
+  };
+  
+  axios.request(options).then(function (response) {
+
+    console.log(response.data);
+    res.json({ name:'wes',dataStuff: response.data})
+  }).catch(function (error) {
+    console.error(error);
+    res.json({ name:'wes',})
+  });
+
+
+
+
+
+
+});
 
 
 
@@ -22,7 +46,7 @@ app.post('/category', async (req,res) =>  {
       console.log(category)
         const options = {
         method: 'GET',
-        url: 'http://bing-news-search1.p.rapidapi.com/news/search',
+        url: 'https://bing-news-search1.p.rapidapi.com/news/search',
         params: {
         q: `${category}`,
         count: '100',
