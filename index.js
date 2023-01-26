35abada8dbcc32f3b244799a3e92915e526b34de
Wes <claude.w.fountain@gmail.com>
2023-01-26 16:24:21 -0500

    tuning /category endpoint

--- index.js
+++ index.js
@@ -35,8 +35,9 @@ app.use(bodyParser.json());
 
 
  app.post('/category', async (req,res) => {
-  let categoryReqest = JSON.stringify(req.body)
-  console.log('this is categoryReqest '+categoryReqest)
+  let categoryReqest = JSON.parse(req.body);
+  //let categoryReqest = JSON.stringify(req.body)
+  console.log('this is categoryReqest '+categoryReqest.category)
   if (Object.keys(req.body).length === 0) {
     res.status(400).send({ message: "Content cannot be empty" });
     return;
